c4d64e3d771d668ca4ee630ce9207b366423703b
Mohd Revan Renandra Arta Putra <mohdrevanrenandraartaputra@gmail.com>
2023-09-25 18:40:27 +0700

    Horizontal List

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,45 @@
 import 'package:flutter/material.dart';
-void main() {
- runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
  const MyApp({super.key});
  @override
  Widget build(BuildContext context) {
- const title = 'Grid List';
+ const title = 'Horizontal List';
  return MaterialApp(
  title: title,
  home: Scaffold(
  appBar: AppBar(
  title: const Text(title),
  ),
- body: GridView.count(
- // Create a grid with 2 columns. If you change the scrollDirection to
- // horizontal, this produces 2 rows.
- crossAxisCount: 2,
- // Generate 100 widgets that display their index in the List.
- children: List.generate(100, (index) {
- return Center(
- child: Text(
- 'Item $index',
- style: Theme.of(context).textTheme.headline5,
+ body: Container(
+ margin: const EdgeInsets.symmetric(vertical: 20.0),
+ height: 200.0,
+ child: ListView(
+ // This next line does the trick.
+ scrollDirection: Axis.horizontal,
+ children: <Widget>[
+ Container(
+ width: 160.0,
+ color: Colors.red,
  ),
- );
- }),
+ Container(
+ width: 160.0,
+ color: Colors.blue,
+ ),
+ Container(
+ width: 160.0,
+ color: Colors.green,
+ ),
+ Container(
+ width: 160.0,
+ color: Colors.yellow,
+ ),
+ Container(
+ width: 160.0,
+ color: Colors.orange,
+ ),
+ ],
+ ),
  ),
  ),
  );
